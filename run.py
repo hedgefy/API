191d4798523c35d5ebb014195a8f433a484200d3
draichi <lucasdraichi@gmail.com>
2018-09-04 14:47:10 -0300

    ping

--- run.py
+++ run.py
@@ -37,5 +37,10 @@ def prophet():
         'yhat_upper': df_forecast['yhat_upper'].tolist(),
         'yhat_lower': df_forecast['yhat_lower'].tolist()
     }), 201
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return 'to the moon'
+
 port = int(os.environ.get('PORT', 5000))
-app.run(host='127.0.0.1', port=port)
+app.run(host='0.0.0.0', port=port)
